e959749a3ecf55c2e0a5ec018ca2a0e960c5a82e
johnli360 <johnli@student.chalmers.se>
2021-03-15 21:20:35 +0100

    Listen for both local and lan requests

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,40 @@
 use std::net::TcpListener;
 use podaemon::read_lines;
+use std::thread;
 
 mod player;
 use player::*;
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
 
-    let listener = TcpListener::bind("127.0.0.1:51234").unwrap();
+    let listener_local = TcpListener::bind("127.0.0.1:51234")?;
+    let listener_lan   = TcpListener::bind("192.168.0.98:51234")?;
+
+    let local_thread = thread::spawn(|| listen(listener_local));
+    let lan_thread = thread::spawn(|| listen(listener_lan));
+
+    local_thread.join().unwrap();
+    lan_thread.join().unwrap();
+    Ok(())
+
+}
+
+fn listen(listener : TcpListener) {
+
     println!("listening on: {}", listener.local_addr().unwrap());
-
     for stream in listener.incoming() {
         match stream {
             Ok(s) => {
-                    print!("connected!");
+                print!("connected!");
 
-                    let lines = read_lines(s);
-                    lines.iter().for_each(|x| println!("line: {}", x));
+                let lines = read_lines(s);
+                lines.iter().for_each(|x| println!("line: {}", x));
 
-                    play(lines);
-                    print!("player finished");
+                play(lines);
+                print!("player finished");
 
                 } ,
             Err(e) => print!("Err: {}", e),
         }
     }
-
-    drop(listener);
 }
